6c57065927213851a82b1f810b4232e91525d708
Pavel Pauliushchyk <p.pavliushchik@aisnovations.com>
2018-03-02 17:38:37 +0300

    components

--- dist/Button/index.js
+++ dist/Button/index.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -7,13 +7,17 @@ exports.default = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var _style = require("styled-jsx/style");
-
-var _style2 = _interopRequireDefault(_style);
-
-var _react = require("react");
+var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
+
+var _Button = require('grommet/components/Button');
+
+var _Button2 = _interopRequireDefault(_Button);
+
+var _Spinning = require('grommet/components/icons/Spinning');
+
+var _Spinning2 = _interopRequireDefault(_Spinning);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -23,17 +27,17 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Button = function (_Component) {
-  _inherits(Button, _Component);
+var CButton = function (_Component) {
+  _inherits(CButton, _Component);
 
-  function Button() {
-    _classCallCheck(this, Button);
+  function CButton() {
+    _classCallCheck(this, CButton);
 
-    return _possibleConstructorReturn(this, (Button.__proto__ || Object.getPrototypeOf(Button)).apply(this, arguments));
+    return _possibleConstructorReturn(this, (CButton.__proto__ || Object.getPrototypeOf(CButton)).apply(this, arguments));
   }
 
-  _createClass(Button, [{
-    key: "render",
+  _createClass(CButton, [{
+    key: 'render',
     value: function render() {
       var _props = this.props,
           children = _props.children,
@@ -41,23 +45,33 @@ var Button = function (_Component) {
           color = _props.color;
 
 
-      return _react2.default.createElement(
-        "button",
-        {
-          style: { backgroundColor: color },
-          onClick: onClick,
-          className: "jsx-975318052" + " " + "button"
-        },
-        children,
-        _react2.default.createElement(_style2.default, {
-          styleId: "975318052",
-          css: [".button.jsx-975318052{border:none;}"]
-        })
+      return (
+        // <div>
+        //   <Button label="Label" onClick={() => {}} className="button">
+        //     <Spinning />
+        //   </Button>
+
+        //   <style jsx>{`
+        //     div :global(.button) {
+        //       color: ${color};
+        //     }
+        //   `}</style>
+        // </div>
+
+        _react2.default.createElement(
+          _Button2.default,
+          {
+            label: 'Label',
+            onClick: function onClick() {},
+            style: { backgroundColor: color }
+          },
+          _react2.default.createElement(_Spinning2.default, null)
+        )
       );
     }
   }]);
 
-  return Button;
+  return CButton;
 }(_react.Component);
 
-exports.default = Button;
+exports.default = CButton;
--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _Button = require('./components/Button');
+var _Button = require('./Button');
 
 Object.defineProperty(exports, 'Button', {
   enumerable: true,
